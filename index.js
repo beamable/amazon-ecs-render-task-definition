902c003b9137e360b6b561f9e1234152b404e003
Justin <jstnlefebvre@gmail.com>
2023-06-02 11:16:50 -0400

    ugh javascript

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ async function run() {
       });
 
       const engine = new Liquid();
-      const text = fs.readFileSync(taskDefPath);
+      const text = fs.readFileSync(taskDefPath, { encoding: 'utf8', flag: 'r' });
       console.log("Text: " + text);
       rendered = engine.parseAndRenderSync(text, context);
     }
